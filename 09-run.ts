4fa92fc95d93eff0db37f26fa4d308bb4e941cb6
Ondřej Frei <ondrej.frei@lokalise.com>
2023-05-17 13:29:21 +0200

    Add 09-run.ts

---
+++ 09-run.ts
@@ -0,0 +1,6 @@
+class Run {
+    constructor() {
+        console.log('In a project where you have some tests, navigate to one of the tests and invoke `Run...`. The test will run inside Phpstorm, or you might be presented with "Edit configurations" dialog (see https://www.jetbrains.com/help/phpstorm/run-debug-configuration.html for help.)');
+        console.log('This is compatible with multiple JS test frameworks and PHP test frameworks, but you can also add a custom run configuration for practically anything e.g. using a shell, so you can quickly trigger a phpstan analysis for example.');
+    }
+}
