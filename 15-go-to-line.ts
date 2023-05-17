405f78d752834a01016c6cbd0adfc7f09e0f4ec1
Ondřej Frei <ondrej.frei@lokalise.com>
2023-05-17 10:24:16 +0200

    Add 15-go-to-line.ts

---
+++ 15-go-to-line.ts
@@ -0,0 +1,22 @@
+class GoToLine {
+    constructor() {
+        console.log('I have line numbers hidden (click the bar with them, select Appearance > Show Line Numbers).');
+        console.log('To quickly jump to e.g. line 20, invoke `Go to Line/Column...`, fill in 20 and hit enter.');
+        console.log('To go to line 3 column 105, invoke `Go to Line/Column...`, fill in 3:105 and hit enter.');
+
+        console.log('This also works with different actions. Invoke `Go to class`, write "cocust:8:54" and hit enter.');
+        console.log('Btw as you might have noticed, no need to type CompleteCurrentStatement - just name a few of the capital letters! This is a huge time saver.');
+    }
+
+    methodA() {
+
+    }
+
+    methodB() {
+
+    }
+
+    methodC() {
+
+    }
+}
