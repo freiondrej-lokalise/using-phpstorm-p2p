62315cf935c1dd2b700b7d19814407ed9da0ec49
Ondřej Frei <ondrej.frei@lokalise.com>
2023-05-17 13:16:38 +0200

    Add 14-back-and-forward.ts

---
+++ 14-back-and-forward.ts
@@ -0,0 +1,15 @@
+class BackAndForward {
+    constructor() {
+        console.log('This has been a super helpful feature for me. Jump to a different file and then invoke `Navigate / Back`. You will return here. Then invoke `Navigate / Forward` and you will return here.');
+        console.log('It is very practical for working in a single file, too. Add a comment inside the body of methodOne(), then add a comment inside the body of methodTwo(). Then invoke `Navigate / Back` and `Navigate / Forward` a few times to get familiar with this.');
+        console.log('You can go back and forward several steps, inside the same file or across multiple files. It might seem confusing at first, but from my experience, the brain holds a pretty nice map of what you have been working on, so this function plays nicely along this.');
+    }
+
+    methodOne() {
+
+    }
+
+    methodTwo() {
+
+    }
+}
