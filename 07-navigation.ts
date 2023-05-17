f430d2c71d2854e73b1a2a3affc3d084b5f7e537
Ondřej Frei <ondrej.frei@lokalise.com>
2023-05-17 13:08:16 +0200

    Add 07-navigation.ts

---
+++ 07-navigation.ts
@@ -0,0 +1,8 @@
+//View < Appearance < Navigation bar
+
+class Navigation {
+    constructor() {
+        console.log('When in the editor, invoke `Jump to Navigation Bar` and use cursor arrows and Enter key to navigate around.');
+        console.log('You can even hide the Navigation bar in View > Appearance > Navigation bar. When you then invoke `Jump to Navigation Bar`, an in-place one will appear. However, when you are sharing your screen, it might be helpful to have the Navigation bar visible.');
+    }
+}
