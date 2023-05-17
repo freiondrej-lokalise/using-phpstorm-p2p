7525f0019baf6675773bf5e3f4c66c8c4224f61f
Ondřej Frei <ondrej.frei@lokalise.com>
2023-05-17 13:34:42 +0200

    Add 12-live-templates.ts

---
+++ 12-live-templates.ts
@@ -0,0 +1,14 @@
+class LiveTemplates {
+    constructor() {
+        console.log('For some repetitive code you usually write, you can create your own live templates. You can find them in Settings > Editor > Live Templates, where you can also see some prebuilt ones.');
+        console.log('For example, I have .cl for console log.');
+        console.log('Let\'s try one of the prebuilt ones now! Navigate to the blank line behind this one, type "fori" and hit Enter. An iteration loop will be created. After writing a value, press Enter and notice how you jump to the next input position.');
+        console.log('The template looks like this:');
+        /*
+        for($VARKIND$ $INDEX$ = 0; $INDEX$ < $LIMIT$; $INDEX$++) {
+         $END$
+        }
+        */
+        console.log('I recommend you to experiment with this a little bit and whenever you find yourself typing some repetitive code, consider creating a nice Live template for it.');
+    }
+}
