039b8d04aa864c5a0844489a17bb0d0c3e6251e0
Ondřej Frei <ondrej.frei@lokalise.com>
2023-05-17 09:52:10 +0200

    Add 08-generate.ts

---
+++ 08-generate.ts
@@ -0,0 +1,12 @@
+class Generate {
+    private int: number;
+
+    private string: string;
+
+    /*
+     * We don't have a constructor here. Invoke `Generate...`, then select Constructor and hit Enter. Select both properties with Shift and hit Enter again. Voilà!
+     * Now invoke `Generate...` again, this time select Getter. Again, with Shift select both properties and hit Enter. You can see that not only the getters were generated, but the property names were updated not to generate a TS error.
+     *
+     * The code being generated is most likely customizable, although I'm not sure where.
+     */
+}
